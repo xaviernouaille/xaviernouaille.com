15157d6c756be84d98ea68d35a926f13aaca7243
Xavier Nouaille <xavier-nouaille@lyknowledge.io>
2021-11-25 00:35:17 +0100

    add mim protection

--- next.config.js
+++ next.config.js
@@ -1,7 +1,25 @@
+// Prévention MIM Security
+const securityHeaders = [
+    {
+        key: 'Strict-Transport-Security',
+        value: 'max-age=63072000; includeSubDomains; preload'
+    }
+]
+  
+
 module.exports = {
     images: {
         loader: "imgix",
         path: "https://xaviernouaille.com",
         domaines: ['xaviernouaille.com']
-    }
+    },
+    async headers() {
+        return [
+          {
+            // Apply these headers to all routes in your application.
+            source: '/(.*)',
+            headers: securityHeaders,
+          },
+        ]
+      },
 }
