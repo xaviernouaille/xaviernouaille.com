9b3220274e1cb86bbe84866ee496ed1f683abe65
Xavier Nouaille <xavier-nouaille@lyknowledge.io>
2021-11-24 03:14:27 +0100

    test fix font

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,7 @@ module.exports = {
         "sm2": "0.925rem"
       },
       fontFamily: {
-        sans: ['"Inter"', ...defaultTheme.fontFamily.sans],
+        sans: ['Inter', ...defaultTheme.fontFamily.sans],
         logo: ['Rock Salt']
       }
     },
