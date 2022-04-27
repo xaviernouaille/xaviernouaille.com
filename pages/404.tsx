df2796f29ca4f619ac80920963733fce2d01edc6
Xavier Nouaille <xavier.nouaille@protonmail.com>
2022-04-27 16:48:05 +0200

    404 update style

--- pages/404.tsx
+++ pages/404.tsx
@@ -15,10 +15,7 @@ export default function notFound404() {
   return (
     <section className='h-screen text-white flex flex-col space-y-5 justify-center items-center'>
       <div className='relative'>
-        <h1 className='md:text-5xl lg:text-6.5xl text-4xl font-bold md:max-w-5xl'>
-          404
-        </h1>
-        <BiMessageSquareError className='absolute -top-5 -right-5 h-7 w-7 text-secondary' />
+        <h1 className='leading-tight'>404</h1>
       </div>
       <p className='text-lg'>Redirection vers l'accueil dans {counter} s</p>
       <div className='flex space-x-5 text-xl'>
