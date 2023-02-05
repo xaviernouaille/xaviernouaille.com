8313060120a902688491bed67c8433a7f6491a71
Xavier Nouaille <xnouaille@dial-once.com>
2023-02-05 01:36:39 +0100

    :chart_with_upwards_trend: add ga script

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,20 +3,40 @@ import '@public/fonts/fonts.css'
 import 'i18n'
 import { CssBaseline, ThemeProvider } from '@mui/material'
 import Header from '@components/Header'
-import { Container } from '@mui/system'
 import Footer from '@components/Footer'
 import { muiThemeDark, muiThemeLight } from 'muiTheme'
 import useSwitchTheme from '@helpers/useSwitchTheme'
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }: AppProps) {
     const [theme, toggleTheme] = useSwitchTheme()
     return (
-        <ThemeProvider theme={theme === 'light' ? muiThemeLight : muiThemeDark}>
-            <CssBaseline />
-            <Header toggleTheme={toggleTheme} />
-            <Component {...pageProps} />
-            <Footer />
-        </ThemeProvider>
+        <>
+            <Script
+                strategy="afterInteractive"
+                src="https://www.googletagmanager.com/gtag/js?id=G-PR0RYWYEZJ"
+            />
+            <Script
+                id="google-analytics"
+                strategy="afterInteractive"
+                dangerouslySetInnerHTML={{
+                    __html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', 'G-PR0RYWYEZJ');
+                    `,
+                }}
+            />
+            <ThemeProvider
+                theme={theme === 'light' ? muiThemeLight : muiThemeDark}
+            >
+                <CssBaseline />
+                <Header toggleTheme={toggleTheme} />
+                <Component {...pageProps} />
+                <Footer />
+            </ThemeProvider>
+        </>
     )
 }
 
