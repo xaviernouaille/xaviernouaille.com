572a6fe52debd07e17fbe2e91c821c2a195a4c61
Xavier Nouaille <xnouaille@dial-once.com>
2024-03-10 23:39:17 +0100

    add resources

--- constants/sources.ts
+++ constants/sources.ts
@@ -6,4 +6,16 @@ export const SOURCES = [
     date: '3/03/2024',
     tags: ['Devops', 'FullStack'],
   },
+  {
+    url: 'https://medium.com/@yecaicedo/structuring-a-node-js-project-with-hexagonal-architecture-7be2ef1364e2',
+    title: 'Structuring a Node.js Project with Hexagonal Architecture',
+    date: '10/03/2024',
+    tags: ['FullStack'],
+  },
+  {
+    url: 'https://www.youtube.com/watch?v=f7Su4KoqSio',
+    title: 'Clean Architecture in TypeScript',
+    date: '10/03/2024',
+    tags: ['FullStack'],
+  },
 ];
