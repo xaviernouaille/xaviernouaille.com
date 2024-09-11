e95e49a46371b9102e0aa75dac6ef1cfc466adb9
Xavier Nouaille <xnouaille@dial-once.com>
2024-09-11 23:46:22 +0200

    feat(header): add icons in header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import clsx from 'clsx';
 import localFont from 'next/font/local';
 import Link from 'next/link';
 import Footer from './components/Footer';
+import { Phone, AtSign } from 'lucide-react';
 
 const myFont = localFont({ src: '../../public/font/my-font.woff2' });
 
@@ -15,7 +16,7 @@ export default function RootLayout({
     <html lang='en'>
       <body className={clsx(myFont.className, 'bg-[#F7FAFC]')}>
         <div className='sticky top-6 z-30 -mx-px transition duration-75 will-change-transform opacity-100'>
-          <div className='bg-slate-100/30 flex justify-between items-center max-w-[900px] mx-auto rounded-2xl py-2 pl-3 pr-6 shadow-surface-glass backdrop-blur'>
+          <div className='bg-slate-100/30 flex justify-between items-center max-w-[900px] mx-2 sm:mx-auto rounded-2xl py-2 pl-3 pr-4 md:pr-6 shadow-surface-glass backdrop-blur'>
             <Link href='/'>
               <img
                 src='https://media.licdn.com/dms/image/v2/D4E03AQG9WfSmyJRX1w/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1723508921407?e=1731542400&v=beta&t=Ho4p7LwvnWMufmdLRhWAB5PK-QS20zqU_sulKX9Zxf0'
@@ -24,7 +25,7 @@ export default function RootLayout({
               />
             </Link>
             <div>
-              <ul className='flex items-center space-x-6 text-slate-600'>
+              <ul className='flex items-center space-x-5 md:space-x-6 text-slate-600'>
                 <Link href='/about'>A propos</Link>
                 <Link
                   target='_blank'
@@ -32,7 +33,12 @@ export default function RootLayout({
                 >
                   Malt
                 </Link>
-                <Link href='tel:+33778389134'>+33 7 78 38 91 34</Link>
+                <Link href='tel:+33778389134'>
+                  <Phone size={16} />
+                </Link>
+                <Link href='mailto:xavier.nouaille@pm.me'>
+                  <AtSign size={16} />
+                </Link>
               </ul>
             </div>
           </div>
